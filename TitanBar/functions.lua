93b662b8ae54cd5af20d9c06bd6a4e9618134dcb
Billy Van Ark <4hchm4xub2@snkmail.com>
2024-05-14 22:30:36 +0200

    Added dump() function

--- TitanBar/functions.lua
+++ TitanBar/functions.lua
@@ -2,6 +2,20 @@
 -- Written By Habna
 -- rewritten by many
 
+-- Basic debug function to look at a table:
+function dump(o)
+    if type(o) == 'table' then
+       local s = '{ '
+       for k,v in pairs(o) do
+          if type(k) ~= 'number' then k = '"'..k..'"' end
+          s = s .. '['..k..'] = ' .. dump(v) .. ','
+       end
+       return s .. '} '
+    else
+       return tostring(o)
+    end
+end
+--
 
 function AddCallback(object, event, callback)
     if (object[event] == nil) then
