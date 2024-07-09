a56daa63de7b394eb75f4cb1135edb4a13acf05d
Billy Van Ark <4hchm4xub2@snkmail.com>
2024-07-09 21:00:29 +0200

    Moved mouse handlers from icon and label to parent control.

--- TitanBar/Control/WalletControlFactory.lua
+++ TitanBar/Control/WalletControlFactory.lua
@@ -28,36 +28,23 @@ function MakeWalletControl(key)
 
     -- We haven't made the control yet, proceed:
     _G[code] = {};
-    MakeWalletControl_Control(_G[code], key);
-    MakeWalletControl_Icon(_G[code], code, key);
-    MakeWalletControl_Label(_G[code], code, key);
+    MakeWalletControl_Control(_G[code], code, key);
+    MakeWalletControl_Icon(_G[code], key);
+    MakeWalletControl_Label(_G[code], key);
 end
 
 _G.MakeWalletControl = MakeWalletControl;
 
-function MakeWalletControl_Control(table, key)
+function MakeWalletControl_Control(table, code, key)
     --**v Control for this currency v**
     table["Ctr"] = Turbine.UI.Control();
     table["Ctr"]:SetParent( TB["win"] );
-    table["Ctr"]:SetMouseVisible( false );
     table["Ctr"]:SetZOrder( 2 );
     table["Ctr"]:SetBlendMode( Turbine.UI.BlendMode.AlphaBlend );
     table["Ctr"]:SetBackColor( Turbine.UI.Color( BC.Alpha[key], BC.Red[key], BC.Green[key], BC.Blue[key] ) );
-    --**^
-end
 
-function MakeWalletControl_Icon(table, code, key)
-    local iconWidth = MakeWalletControl_IconWidthOverrides[key] or 32;
-    local iconHeight = MakeWalletControl_IconHeightOverrides[key] or 32;
-
-    table["Icon"] = Turbine.UI.Control();
-    table["Icon"]:SetParent( table["Ctr"] );
-    table["Icon"]:SetBlendMode( Turbine.UI.BlendMode.AlphaBlend );
-    table["Icon"]:SetSize( iconWidth, iconHeight );
-    table["Icon"]:SetBackground( WalletItem[key].Icon );
-
-    table["Icon"].MouseMove = function( sender, args )
-        table["Lbl"].MouseLeave( sender, args );
+    table["Ctr"].MouseMove = function( sender, args )
+        table["Ctr"].MouseLeave( sender, args );
         TB["win"].MouseMove();
         if dragging then
             MoveCtr(table, args);
@@ -66,48 +53,11 @@ function MakeWalletControl_Icon(table, code, key)
         end
     end
 
-    table["Icon"].MouseLeave = function( sender, args )
-        table["Lbl"].MouseLeave( sender, args );
-    end
-
-    table["Icon"].MouseClick = function( sender, args )
-        table["Lbl"].MouseClick( sender, args );
-    end
-
-    table["Icon"].MouseDown = function( sender, args )
-        table["Lbl"].MouseDown( sender, args );
-    end
-
-    table["Icon"].MouseUp = function( sender, args )
-        table["Lbl"].MouseUp( sender, args );
-    end
-end
-
-function MakeWalletControl_Label(table, code, key)
-    local labelAlignment = MakeWalletControl_LabelAlignmentOverrides[key] or Turbine.UI.ContentAlignment.MiddleRight;
-
-    table["Lbl"] = Turbine.UI.Label();
-    table["Lbl"]:SetParent( table["Ctr"] );
-    table["Lbl"]:SetFont( _G.TBFont );
-    table["Lbl"]:SetPosition( 0, 0 );
-    table["Lbl"]:SetFontStyle( Turbine.UI.FontStyle.Outline );
-    table["Lbl"]:SetTextAlignment( labelAlignment );
-
-    table["Lbl"].MouseMove = function( sender, args )
-        table["Lbl"].MouseLeave( sender, args );
-        TB["win"].MouseMove();
-        if dragging then
-            MoveCtr(table, args);
-        else
-            ShowToolTipWin( code );
-        end
-    end
-
-    table["Lbl"].MouseLeave = function( sender, args )
+    table["Ctr"].MouseLeave = function( sender, args )
         ResetToolTipWin();
     end
 
-    table["Lbl"].MouseClick = function( sender, args )
+    table["Ctr"].MouseClick = function( sender, args )
         TB["win"].MouseMove();
         if ( args.Button == Turbine.UI.MouseButton.Left ) then
             if not WasDrag then
@@ -120,7 +70,7 @@ function MakeWalletControl_Label(table, code, key)
         WasDrag = false;
     end
 
-    table["Lbl"].MouseDown = function( sender, args )
+    table["Ctr"].MouseDown = function( sender, args )
         if ( args.Button == Turbine.UI.MouseButton.Left ) then
             table["Ctr"]:SetZOrder( 3 );
             dragStartX = args.X;
@@ -129,7 +79,7 @@ function MakeWalletControl_Label(table, code, key)
         end
     end
 
-    table["Lbl"].MouseUp = function( sender, args )
+    table["Ctr"].MouseUp = function( sender, args )
         table["Ctr"]:SetZOrder( 2 );
         dragging = false;
         Position.Left[key] = table["Ctr"]:GetLeft();
@@ -138,6 +88,31 @@ function MakeWalletControl_Label(table, code, key)
         settings[key].Y = string.format("%.0f", Position.Top[key]);
         SaveSettings( false );
     end
+    --**^
+end
+
+function MakeWalletControl_Icon(table, key)
+    local iconWidth = MakeWalletControl_IconWidthOverrides[key] or 32;
+    local iconHeight = MakeWalletControl_IconHeightOverrides[key] or 32;
+
+    table["Icon"] = Turbine.UI.Control();
+    table["Icon"]:SetParent( table["Ctr"] );
+    table["Icon"]:SetBlendMode( Turbine.UI.BlendMode.AlphaBlend );
+    table["Icon"]:SetSize( iconWidth, iconHeight );
+    table["Icon"]:SetBackground( WalletItem[key].Icon );
+    table["Icon"]:SetMouseVisible(false);
+end
+
+function MakeWalletControl_Label(table, key)
+    local labelAlignment = MakeWalletControl_LabelAlignmentOverrides[key] or Turbine.UI.ContentAlignment.MiddleRight;
+
+    table["Lbl"] = Turbine.UI.Label();
+    table["Lbl"]:SetParent( table["Ctr"] );
+    table["Lbl"]:SetFont( _G.TBFont );
+    table["Lbl"]:SetPosition( 0, 0 );
+    table["Lbl"]:SetFontStyle( Turbine.UI.FontStyle.Outline );
+    table["Lbl"]:SetTextAlignment( labelAlignment );
+    table["Lbl"]:SetMouseVisible(false);
 end
 
 function MoveCtr(table, args)
@@ -145,7 +120,7 @@ function MoveCtr(table, args)
     local CtrWidth = table["Ctr"]:GetWidth();
     CtrLocX = CtrLocX + ( args.X - dragStartX );
     if CtrLocX < 0 then CtrLocX = 0; elseif CtrLocX + CtrWidth > screenWidth then CtrLocX = screenWidth - CtrWidth; end
-    
+
     local CtrLocY = table["Ctr"]:GetTop();
     local CtrHeight = table["Ctr"]:GetHeight();
     CtrLocY = CtrLocY + ( args.Y - dragStartY );
